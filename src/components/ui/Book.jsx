5b2c916f6a9a45cc5a8fc2e1c1c42256e1f47e8b
Abhay <26abhaykumarsingh@gmail.com>
2023-05-21 18:25:22 +0530

    add bookinfo and cart routes

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,40 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Price from "./Price";
 
 export default function Book({ book }) {
   return (
     <div className="book">
-      <Link to="">
+      <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img src={book.url} />
         </figure>
       </Link>
       <div className="book__title">
-        <Link to="/" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
-      <div className="book__ratings">
-        {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
-          <FontAwesomeIcon icon="star" key={index} />
-        ))}
-        {!Number.isInteger(book.rating) && (
-          <FontAwesomeIcon icon="star-half-alt" />
-        )}
-      </div>
-      <div className="book__price">
-        {book.salePrice ? (
-          <>
-            <span className="book__price--normal">
-              ${book.originalPrice.toFixed(2)}
-            </span>{" "}
-            ${book.salePrice.toFixed(2)}
-          </>
-        ) : (
-          <>${book.originalPrice}</>
-        )}
-      </div>
+      <Rating rating={book.rating} />
+      <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
     </div>
   );
 }
